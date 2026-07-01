33f086811ea2310eec8c30328054a2f69f0a7dd1
LEONARDO-LAPTOP\leobu <leobuncec@gmail.com>
2026-07-01 11:48:12 +0200

    latest changes

--- frontend/lib/core/constants/app_colors.dart
+++ frontend/lib/core/constants/app_colors.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class AppColors {
   static const primary = Color(0xFFFFD54F);
   static const background = Color.fromARGB(255, 255, 235, 171);
   static const card = Color(0xFFFFF176);
   static const border = Color(0xFFFFE082);
-  static const shadow = Colors.black12;
+  static const shadow = Color.fromRGBO(0, 0, 0, 0.12);
   static const selectedCard = Color.fromARGB(255, 255, 223, 116);
   static const text = Color.fromARGB(255, 0, 0, 0);
 }
